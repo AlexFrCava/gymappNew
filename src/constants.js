15c79de6a727347859d000bb2d8b902208feea20
AlexFrCava <112496181+AlexFrCava@users.noreply.github.com>
2023-05-07 04:46:28 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const Customer_API= "https://traineeapp.azurewebsites.net/api/customers"
-export const Exercises_API= "https://traineeapp.azurewebsites.net/api/trainings"
+export const Customer_API= "http://traineeapp.azurewebsites.net/api/customers"
+export const Exercises_API= "http://traineeapp.azurewebsites.net/api/trainings"
 export const Full_API="https://traineeapp.azurewebsites.net/gettrainings"
